571722ab400e0f8cf5f9eddaea103b0af69949f9
Hendro Steven <hendro.steven@gmail.com>
2020-09-30 15:49:22 +0700

    add save category

--- src/main/java/com/bca/api/controller/CategoryController.java
+++ src/main/java/com/bca/api/controller/CategoryController.java
@@ -4,10 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bca.dto.CategoryForm;
 import com.bca.dto.ResponseData;
+import com.bca.entity.Category;
 import com.bca.services.CategoryService;
 
 @RestController
@@ -30,5 +34,15 @@ public class CategoryController {
 			response.getMessages().add("Could not load categories: " + ex.getMessage());
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
 		}
+	}
+	
+	@PostMapping
+	public ResponseEntity<ResponseData> saveCategory(@RequestBody CategoryForm form){
+		Category category = new Category();
+		category.setName(form.getName());
+		ResponseData response = new ResponseData();
+		response.setStatus(true);
+		response.setPayload(categoryService.save(category));
+		return ResponseEntity.ok(response);
 	}
 }
